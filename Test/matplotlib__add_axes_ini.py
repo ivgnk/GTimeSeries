c9cd85240026393315c2f885a31727d0003104e3
Ivan Genik <IVGenik@rambler.ru>
2024-02-27 20:33:58 +0500

    Add files via upload

--- Test/matplotlib__add_axes_ini.py
+++ Test/matplotlib__add_axes_ini.py
@@ -5,6 +5,14 @@ https://pythonguides.com/add_axes-matplotlib/
 
 import matplotlib.pyplot as plt
 fig = plt.figure()
-axes = fig.add_axes(rect=(0.1, 0.1, 0.85, 0.85))
-plt.grid()
+i = 1
+match i:
+    case 0:
+         axes = fig.add_axes(rect=(0.1, 0.1, 0.85, 0.85))
+         plt.grid()
+    case 1:
+        axes1 = fig.add_axes(rect=(0.1, 0.1, 0.35, 0.85))
+        plt.grid()
+        axes2 = fig.add_axes(rect=(0.6, 0.1, 0.35, 0.85))
+        plt.grid()
 plt.show()
