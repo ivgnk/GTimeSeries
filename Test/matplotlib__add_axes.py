007e35cf6ba98e206a4acb3077311b20fff057f9
Ivan Genik <IVGenik@rambler.ru>
2024-03-15 00:46:35 +0500

    Add files via upload

--- Test/matplotlib__add_axes.py
+++ Test/matplotlib__add_axes.py
@@ -11,16 +11,16 @@ fig = plt.figure()
 
 x = np.arange(0, 20, 0.2)
 y1 = np.sin(x)
-y2 = np.sqrt(x)
+y2 = np.sqrt(x)*250
 
 # https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.add_axes
 # recttuple (left, bottom, width, height)
 axes1 = fig.add_axes(rect=(0.1, 0.1, 0.8, 0.4))
 plt.grid()
-axes1.plot(x, y1)
+axes1.plot(x, y1) # sin
 
 axes2 = fig.add_axes(rect=(0.1, 0.6, 0.8, 0.4))
-axes2.plot(x, y2)
+axes2.plot(x, y2) # sqrt
 plt.grid()
 
 plt.show()
