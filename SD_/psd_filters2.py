c39ec78a11266f6edca8971ab41e45b9b774220a
Ivan Genik <IVGenik@rambler.ru>
2024-03-27 20:26:56 +0500

    Add files via upload

--- SD_/psd_filters2.py
+++ SD_/psd_filters2.py
@@ -3,6 +3,8 @@
 Преимущественно фильтры
 '''
 import inspect
+import sys
+
 import numpy as np
 import scipy as sc
 # from bp_const import *
@@ -15,9 +17,15 @@ from pstring import *
 import pandas as pd
 # from astropy.modeling.models import *  # для задания гауссова распределения весов окна
 
+from scipy import signal
+from scipy.signal import butter, bessel, cheby1, cheby2, ellip
+
+
+
 # Для фильтраций временных рядов
 SMA_filtname =['Moving average','Moving average with weights (triangle)','Savitzky–Golay filter','Wiener filter']
-SOS_filtname = ['Butterworth filter']
+SOS_filtname = ['Butterworth filter','Bessel filter']
+ln_style = ['solid','dashed','dotted','dashdot']
 SMA_all_w = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
 SMA_w: Final = [3, 7, 13, 25, 51]  # SMA - Simple Moving  Average,  размер окон
 SMA_wt = tuple(SMA_w)
@@ -58,7 +66,7 @@ def test_smoothing():
     res = work_with_smoothing(x, y)
 
 
-def work_with_smoothing2(x: np.ndarray, y: np.ndarray, with_edges=False)->(int, int, np.ndarray, list):
+def work_with_smoothing2sos(x: np.ndarray, y: np.ndarray, with_edges=False)->(int, int, np.ndarray, list):
     '''
     Сглаживание различными фильтрами
     Return:
@@ -77,13 +85,13 @@ def work_with_smoothing2(x: np.ndarray, y: np.ndarray, with_edges=False)->(int, 
     curr_flt = 0
     num_flt = 0
     llst =[]
-    # ---- (00) - усреднение равновесовое
+    # ---- (00) - 'butter'
     (num_flt, curr_flt, res, llst) = prep_my_butter1D_filter(num_flt, curr_flt, y, res, llst, with_edges)
-    # # ---- (01) - усреднение c треугольными весами
-    # (num_flt, curr_flt, res, llst) = prep_my_moving_average1D_filter_ww(num_flt, curr_flt, y, res, llst, with_edges)
-    # # ---- (O2) - sc.signal.savgol_filter     https://en.wikipedia.org/wiki/Savitzky%E2%80%93Golay_filter
+    # ---- (01) - 'bessel'
+    (num_flt, curr_flt, res, llst) = prep_my_bessel1D_filter(num_flt, curr_flt, y, res, llst, with_edges)
+    # # ---- (O2) -
     # (num_flt, curr_flt, res, llst) = prep_savgol(num_flt, curr_flt, y, res, llst)
-    # # ---- (03) - wiener1D_filter
+    # # ---- (03) -
     # (num_flt, curr_flt, res, llst) = prep_wiener1D_filter(num_flt, curr_flt, y, res, llst)
     return num_flt, curr_flt, res, llst
 
@@ -130,7 +138,38 @@ def prep_butter1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarr
     '''
     Образец tst_butterworth_filter2.py - > Python_Scipy_Filters
     '''
+    num_flt += 1; npvar = 1
+    Wn_ = 20; btype_= 'low' # 'lowpass'
+    for i in range(npvar):
+        res[curr_flt, 0] = num_flt
+        res[curr_flt, 1] = SOS_filtname[0]  # 'Butterworth filter'
+        res[curr_flt, 2] = i + 1
+        sos = butter(N=15, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # butter, bessel
+        filtd = signal.sosfilt(sos, dat)  # dat = signal_
+        res[curr_flt, 3] = filtd
+        # print(f'{filtd=}'); print(f'{dat=}'); input()
+        res[curr_flt, 4] = SOS_filtname[0]+' '+str(i)
+        curr_flt += 1
+    llst += [npvar]
+    return num_flt, curr_flt, res, llst
 
+# Фильтр Бесселя
+def prep_bessel1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray, llst:list)->(int, int, np.ndarray, list):
+    '''
+    Образец tst_butterworth_filter2.py - > Python_Scipy_Filters
+    '''
+    num_flt += 1; npvar = 1
+    Wn_ = 20; btype_= 'lowpass'
+    for i in range(npvar):
+        res[curr_flt, 0] = num_flt
+        res[curr_flt, 1] = SOS_filtname[0]  # 'Butterworth filter'
+        res[curr_flt, 2] = i + 1
+        sos = bessel(N=15, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # butter, bessel
+        filtd = signal.sosfilt(sos, dat)  # dat = signal_
+        res[curr_flt, 3] = filtd
+        res[curr_flt, 4] = SOS_filtname[0]+' '+str(i)
+        curr_flt += 1
+    llst += [npvar]
     return num_flt, curr_flt, res, llst
 
 
@@ -166,23 +205,45 @@ def prep_savgol(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray, llst:
 def prep_my_butter1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray,
                                     llst:list, with_edges=False)->(int, int, np.ndarray, list):
     '''
-    Усреднение без весов (равновесовым фильтром)
+    'Butterworth filter'
     '''
     num_flt += 1
     npvar = 1 # число подвариантов
+    Wn_ = 20;  btype_ = 'lowpass'
     for i in range(npvar):
         win_len = SMA_w[i]
         res[curr_flt, 0] = num_flt
         res[curr_flt, 1] = SOS_filtname[0]  # 'Butterworth filter'
         res[curr_flt, 2] = i + 1
-        #  prep_butter1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray, llst:list)->(int, int, np.ndarray, list):
-        # !!!!
-        # res[curr_flt, 3] = prep_butter1D_filter(num_flt, curr_flt, dat, res, llst) #->(int, int, np.ndarray, list):
-         # my_moving_average1D(dat, window_size=win_len, with_edges=with_edges))
+        sos = butter(N=15, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # butter, bessel
+        filtd = signal.sosfilt(sos, dat)  # dat = signal_
+        res[curr_flt, 3] = filtd
         res[curr_flt, 4] = 'no win'
         curr_flt += 1
     llst += [npvar]
     return num_flt, curr_flt, res, llst
+
+def prep_my_bessel1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray,
+                                    llst:list, with_edges=False)->(int, int, np.ndarray, list):
+    '''
+    'Bessel filter'
+    '''
+    num_flt += 1
+    npvar = 1 # число подвариантов
+    Wn_ = 20;  btype_ = 'lowpass'
+    for i in range(npvar):
+        win_len = SMA_w[i]
+        res[curr_flt, 0] = num_flt
+        res[curr_flt, 1] = SOS_filtname[1]  # 'Bessel filter'
+        res[curr_flt, 2] = i + 1
+        sos = bessel(N=15, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # butter, bessel
+        filtd = signal.sosfilt(sos, dat)  # dat = signal_
+        res[curr_flt, 3] = filtd
+        res[curr_flt, 4] = 'no win'
+        curr_flt += 1
+    llst += [npvar]
+    return num_flt, curr_flt, res, llst
+
 
 
 def prep_my_moving_average1D_filter(num_flt:int, curr_flt:int, dat:np.ndarray, res:np.ndarray,
@@ -278,12 +339,13 @@ def get_subplot_num2(npictures:int)->tuple:
     '''
     Задает разбиение окна на части в зависимости от числа картинок
     '''
+    lim = 16
     if npictures < 1:
         print('Ошибка: Число subplot должно быть больше 0')
         raise ValueError('Число subplot должно быть больше 0')
-    elif npictures >10:
-        print('Ошибка: Число subplot должно быть меньше 11')
-        raise ValueError('Число subplot должно быть меньше 11')
+    elif npictures >lim:
+        print(f'Ошибка: Число subplot должно быть меньше {lim}')
+        raise ValueError(f'Число subplot должно быть меньше {lim}')
 
     match npictures:
         case 1: res = (1,1) # row col
@@ -292,6 +354,9 @@ def get_subplot_num2(npictures:int)->tuple:
         case 5 | 6: res = (2, 3)
         case 7 | 8 | 9: res = (3, 3)
         case 10: res = (2, 5)
+        case 11 | 12: res = (4, 3)
+        case 13 | 14 | 15 : res = (5, 3)
+        case 16 : res = (4, 4)
         case default: res=tuple()
     return res
 
@@ -324,9 +389,38 @@ def tst_create_signals():
     print('Function ',inspect.currentframe().f_code.co_name)
     create_signals(len_filter=140, the_test=True)
 
-def test_filters_with_len_window2(len_filter=140, len_flt_tpl:tuple=SMA_wt, with_legend = True, with_edges=False):
+def test_filters_with_sos2(len_filter=140, with_legend=False, with_edges=bool(1)):
+    x, y = create_signals(len_filter)
+    # Это основная часть, все остальное - для визуализации
+    ########################################################
+    (num_flt, curr_flt, res, llst) = work_with_smoothing2sos(x, y, with_edges)
+    # print(f'{num_flt=}')
+    # print(f'{res=}')
+    ########################################################
+    currflt = 0
+    res_corr_coeff=[]
+    for i in range(num_flt): # перебор по всем фильтрам
+        for j in range(llst[i]): # перебор по всем подвариантам фильтров
+            if res[currflt,4] != '':
+                curr_s = res[currflt, 4]
+                chngd_s = remove_letters(curr_s)
+                # print(f'{i=} {chngd_s=}')
+                # print(f'{y=}')
+                # print(f'{res[currflt, 3]=}')
+                corr_coeff = np.corrcoef(y, res[currflt, 3])
+                # print(f'{corr_coeff=}')
+                # https://www.codecamp.ru/blog/correlation-in-python/
+                cc = corr_coeff[0, 1]
+                # print(i, res[currflt, 1], '  corr_coeff=', cc)
+                res_corr_coeff.append([len_filter,res[currflt, 1],cc])
+                currflt += 1
+    # print(f'Good return')
+    return res_corr_coeff
+
+def test_filters_with_len_window2(len_filter=140, len_flt_tpl:tuple=SMA_wt, with_legend = True, with_edges=False)->list:
     # print('Function = test_filters_with_len_window2')
     # print(f'{len_filter=}')
+    # print('Function ',inspect.currentframe().f_code.co_name)
     x,y = create_signals(len_filter)
     ########################################################
     # Это основная часть, все остальное - для визуализации
@@ -390,11 +484,9 @@ def test_filters_with_sos(len_filter=140, with_legend=False, with_edges=bool(1))
     x,y = create_signals(len_filter)
     ########################################################
     # Это основная часть, все остальное - для визуализации
-    (num_flt, curr_flt, res, llst) = work_with_smoothing2(x, y,with_edges)
+    (num_flt, curr_flt, res, llst) = work_with_smoothing2sos(x, y, with_edges)
     ########################################################
 
-    # print(num_flt, curr_flt, llst)
-    sp = get_subplot_num(num_flt)
     # print(num_flt, curr_flt, llst)
     sp = get_subplot_num(num_flt)
     plt.subplots( nrows = sp[0], ncols = sp[1], figsize=figsize_st) # для качественной вставки в презентацию
@@ -562,7 +654,8 @@ def test_wiener_filter():
     plt.legend()
     plt.show()
 
-def tst_test_filters_with_len_window2():
+def tst_test_filters_with_sos2():
+    print(inspect.currentframe().f_code.co_name)
     lst = list(range(100,6000,40))
     lenlst = len(lst)
     print(lst)
@@ -570,20 +663,35 @@ def tst_test_filters_with_len_window2():
     print(type(df))
     for i,dat in enumerate(lst):
         if i % 20 == 0: print(f'{i} / {lenlst}')
-        lst = test_filters_with_len_window2(len_filter=dat, len_flt_tpl=(13,), with_legend=False, with_edges=bool(1)) # Good
-        for dat2 in lst:
+        lst2 = test_filters_with_sos2(len_filter=dat, with_legend=False, with_edges=bool(1)) # Good
+        for dat2 in lst2:
             df.loc[len(df.index)] = dat2
-    work_with_stat_pd(df)
+    work_with_stat_pd(df,SOS_filtname)
+    print('End of tst_test_filters_with_sos2')
+
+def tst_test_filters_with_len_window2():
+    print('Function ',inspect.currentframe().f_code.co_name)
+    lst = list(range(100,6000,40))
+    lenlst = len(lst)
+    print(lst)
+    df = pd.DataFrame(columns=["Filter length", "Filter name", "Correlation coefficient"])
+    print(type(df))
+    for i,dat in enumerate(lst):
+        if i % 20 == 0: print(f'{i} / {lenlst}')
+        lst2 = test_filters_with_len_window2(len_filter=dat, len_flt_tpl=(13,), with_legend=False, with_edges=bool(1)) # Good
+        for dat2 in lst2:
+            df.loc[len(df.index)] = dat2
+    work_with_stat_pd(df,SMA_filtname)
     print('End of tst_test_filters_with_len_window2')
 
-def work_with_stat_pd(df):
+def work_with_stat_pd(df,flt_names:list):
     print(f'{len(df)}')
     plt.figure(figsize=(12, 8))
-    for i,fn_ in enumerate(SMA_filtname):
+    for i,fn_ in enumerate(flt_names):
         res1 = df.loc[df['Filter name'] == fn_]
         # print(f'{i=}  {len(res1)}')
         # print(res1)
-        plt.plot(res1['Filter length'], res1['Correlation coefficient'], label = fn_)
+        plt.plot(res1['Filter length'], res1['Correlation coefficient'], label = fn_, linestyle=ln_style[i])
     plt.xlabel('Длина выборки', fontsize=12) #
     plt.ylabel('Коэффициент корреляции', fontsize=12)
     plt.grid()
@@ -600,9 +708,11 @@ if __name__ == "__main__":
     #test_calc_triang_weights_for_filter1D()
 
     # tst_test_filters_with_len_window2()
+
+    tst_test_filters_with_sos2()
     # test_filters_with_len_window(140, len_flt_tpl=(13,), with_legend=False, with_edges=bool(1)) #
 
-    test_filters_with_sos(len_filter=140, with_legend=False, with_edges=bool(1))
+    # test_filters_with_sos(len_filter=140, with_legend=False, with_edges=bool(1))
     # test_calc_lst_of_triang_weights()
     # test_wiener_filter() # Good
 
--- SD_/tst_butterworth_filter2.py
+++ SD_/tst_butterworth_filter2.py
@@ -3,6 +3,8 @@ August 29, 2022 Python Scipy Butterworth Filter
 https://pythonguides.com/python-scipy-butterworth-filter/
 '''
 
+from random import *
+
 from scipy import signal
 from scipy.signal import butter, bessel, cheby1, cheby2, ellip
 import numpy as np
@@ -14,9 +16,139 @@ from psd_filters2 import get_subplot_num2
 # Const
 figsize_st = (15, 12)
 figsize_st2 = (10, 8)
+rand_coeff= 2
 flt_type = ['lowpass', 'highpass', 'bandpass', 'bandstop']
 flt_func = [butter, bessel, cheby1, cheby2, ellip]  # , cheby2
 flt_func_name = ['butter', 'bessel', 'cheby1', 'cheby2', 'ellip']  #
+
+
+def testFull_Python_Scipy_Butterworth_Filter(N_Wn:list, n_signal=0, nnums=2000, rand_coeff = 2.0): # Only LowPass
+    # Create the time duration of the signal
+    t_duration = np.linspace(0, 0.5, nnums, endpoint=False)
+    #  Generate a signal of 20 and 40 Hz frequency
+    seed(123); llen = len(t_duration)
+    match n_signal:
+        case 0: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+        case 1: sign = np.array([random()*rand_coeff for i in range(llen)])
+        case 2:
+                sign1 = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+                sign2 = np.array([random()*rand_coeff for i in range(llen)])
+                sign = sign1+sign2
+        case default: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+
+    with plt.ioff():
+        fig1, ax1 = plt.subplots(1, 1, sharex=True, figsize=figsize_st2)
+        if n_signal==2:
+            ax1.plot(t_duration, sign1,label='regular signal',linewidth= 3)
+            ax1.plot(t_duration, sign2,label='random signal')
+        else:
+            ax1.plot(t_duration, sign,label='full signal')
+        ax1.set_title('20 and 40 Hz Sinusoid')
+        ax1.axis([0, 0.5, -2, 2.5])
+        plt.grid()
+        plt.legend()
+        plt.show(block=False)
+
+        # Create a Butterworth high pass filter of 25 Hz and apply it to the above-created signal using the below code.
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+        st=get_subplot_num2(len(N_Wn))
+        print(st)
+        fig2, ax2 = plt.subplots(st[0],st[1], sharex=True, figsize=figsize_st)
+        # fig2.suptitle(f'different N & Wn',color='r')
+        # ax2.axis([0, 0.5, -2, 2.5])
+        for i,dat in enumerate(N_Wn):
+            plt.subplot(st[0], st[1], i + 1)
+            sos = butter(N = dat[0], Wn = dat[1], btype='low', fs=2000, output='sos')
+            filtd = signal.sosfilt(sos, sign)
+
+            plt.plot(t_duration, filtd)
+            plt.title(f'N={dat[0]}  Wn={dat[1]} filter')
+            plt.tight_layout()
+            plt.xlabel('Time (seconds)')
+            plt.grid()
+        plt.show()
+
+def test1Wn_Python_Scipy_Butterworth_Filter(n_signal=0, nnums=2000, rand_coeff= 2): # Only LowPass
+    # Create the time duration of the signal
+    t_duration = np.linspace(0, 0.5, nnums, endpoint=False)
+    #  Generate a signal of 20 and 40 Hz frequency
+    seed(123); llen = len(t_duration)
+    match n_signal:
+        case 0: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+        case 1: sign = np.array([random()*rand_coeff for i in range(llen)])
+        case 2: sign = (np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)+
+                        np.array([random()*rand_coeff for i in range(llen)]))
+        case default: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+
+    with plt.ioff():
+        fig1, ax1 = plt.subplots(1, 1, sharex=True, figsize=figsize_st2)
+        ax1.plot(t_duration, sign)
+        ax1.set_title('20 and 40 Hz Sinusoid')
+        ax1.axis([0, 0.5, -2, 2.5])
+        plt.grid()
+        plt.show(block=False)
+
+        # Create a Butterworth high pass filter of 25 Hz and apply it to the above-created signal using the below code.
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+        Wn_ = [1, 2, 5, 6, 7, 8, 9, 10, 15, 20,30]
+        st=get_subplot_num2(len(Wn_))
+        print(st)
+        fig2, ax2 = plt.subplots(st[0],st[1], sharex=True, figsize=figsize_st)
+        N_ = 5
+        fig2.suptitle(f'{N_=}, different Wn',color='r')
+        # ax2.axis([0, 0.5, -2, 2.5])
+        for i,dat in enumerate(Wn_):
+            plt.subplot(st[0], st[1], i + 1)
+            sos = butter(N = N_, Wn = dat, btype='low', fs=2000, output='sos')
+            filtd = signal.sosfilt(sos, sign)
+
+            plt.plot(t_duration, filtd)
+            plt.title('After applying Wn='+str(dat)+' filter')
+            plt.tight_layout()
+            plt.xlabel('Time (seconds)')
+            plt.grid()
+        plt.show()
+
+def test1N_Python_Scipy_Butterworth_Filter(n_signal=0, nnums=2000, rand_coeff= 2): # Only LowPass
+    # Create the time duration of the signal
+    t_duration = np.linspace(0, 0.5, nnums, endpoint=False)
+    #  Generate a signal of 20 and 40 Hz frequency
+    seed(123); llen = len(t_duration)
+    match n_signal:
+        case 0: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+        case 1: sign = np.array([random()*rand_coeff for i in range(llen)])
+        case 2: sign = (np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)+
+                        np.array([random()*rand_coeff for i in range(llen)]))
+        case default: sign = np.sin(2 * np.pi * 20 * t_duration) + np.sin(2 * np.pi * 40 * t_duration)
+
+    with plt.ioff():
+        fig1, ax1 = plt.subplots(1, 1, sharex=True, figsize=figsize_st2)
+        ax1.plot(t_duration, sign)
+        ax1.set_title('20 and 40 Hz Sinusoid')
+        ax1.axis([0, 0.5, -2, 2.5])
+        plt.grid()
+        plt.show(block=False)
+
+        # Create a Butterworth high pass filter of 25 Hz and apply it to the above-created signal using the below code.
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+        N = [1, 2, 5, 6, 7, 8, 9, 10, 15, 20,30]
+        st=get_subplot_num2(len(N))
+        print(st)
+        fig2, ax2 = plt.subplots(st[0],st[1], sharex=True, figsize=figsize_st)
+        Wn_ = 15
+        fig2.suptitle(f'{Wn_=}, different N',color='r')
+        # ax2.axis([0, 0.5, -2, 2.5])
+        for i,dat in enumerate(N):
+            plt.subplot(st[0], st[1], i + 1)
+            sos = butter(N = dat, Wn = 15, btype='low', fs=2000, output='sos')
+            filtd = signal.sosfilt(sos, sign)
+
+            plt.plot(t_duration, filtd)
+            plt.title('After applying N ='+str(dat)+' filter')
+            plt.tight_layout()
+            plt.xlabel('Time (seconds)')
+            plt.grid()
+        plt.show()
 
 
 def Python_Scipy_Butterworth_Filter():
@@ -96,6 +228,8 @@ def Python_Scipy_Bessel_Filter():
 def Python_Scipy_Filters(num_flt:int, flt_func:function, t_duration:np.ndarray, signal_:np.ndarray):
     '''
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.bessel.html
+    flt_func = [butter, bessel, cheby1, cheby2, ellip]  # , cheby2
+    num_flt          0        1      2       3       4
     '''
     flt_func_name = flt_func.__name__+' filter'
     st=get_subplot_num2(4)
@@ -107,17 +241,17 @@ def Python_Scipy_Filters(num_flt:int, flt_func:function, t_duration:np.ndarray, 
         if (i==0) or (i==1): Wn_ = 20
         else: Wn_ = [10,30]
         match num_flt:
-            case 0 | 1:  sos = flt_func(N = 15, Wn = Wn_, btype=btype_, fs=2000, output='sos')
+            case 0 | 1:  sos = flt_func(N = 15, Wn = Wn_, btype=btype_, fs=2000, output='sos') # butter, bessel
             case 2:
                 rp_ = 5
-                sos = flt_func(N = 15, rp=rp_, Wn = Wn_, btype=btype_, fs=2000, output='sos')
+                sos = flt_func(N = 15, rp=rp_, Wn = Wn_, btype=btype_, fs=2000, output='sos')  # cheby1
             case 3:
                 rs_ = 40
-                sos = flt_func(N=15, rs=rs_, Wn=Wn_, btype=btype_, fs=2000, output='sos')
+                sos = flt_func(N=15, rs=rs_, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # cheby2
             case 4:
                 rp_ = 5
                 rs_ = 40
-                sos = flt_func(N=15, rp=rp_, rs=rs_, Wn=Wn_, btype=btype_, fs=2000, output='sos')
+                sos = flt_func(N=15, rp=rp_, rs=rs_, Wn=Wn_, btype=btype_, fs=2000, output='sos')  # ellip
 
         filtd = signal.sosfilt(sos, signal_)
 
@@ -158,7 +292,11 @@ def test_Python_Scipy_Filters():
             Python_Scipy_Filters(i,flt,t_duration, signal_)
 
 if __name__ == "__main__":
+    N_Wn = [[1,1],[1,2],[1,5],[1,10],[1,15],[1,20],[1,40],[1,60],[1,100]]
+    testFull_Python_Scipy_Butterworth_Filter(N_Wn=N_Wn, n_signal = 2, nnums = 400, rand_coeff = 2)
+    # test1Wn_Python_Scipy_Butterworth_Filter(2,100,1)  # Best of diff var -> N = 5, Wn = 15
+    # test1N_Python_Scipy_Butterworth_Filter(2) # Best of diff var -> N = 5, Wn = 15
     # Python_Scipy_Butterworth_Filter()
     # Python_Scipy_Bessel_Filter()
-    test_Python_Scipy_Filters()
+    # test_Python_Scipy_Filters()
 
